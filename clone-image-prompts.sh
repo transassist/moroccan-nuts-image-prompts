c37f3658a180973a8a2b6ff4527b311519bbaca7
transassist <130241129+transassist@users.noreply.github.com>
2025-03-27 17:56:07 +0000

    Add clone script for convenience

---
+++ clone-image-prompts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change to the project directory
+cd "/Users/tarikkhachiaa/Moroccan_Nuts_Website/"
+
+# Clone the repository
+git clone https://github.com/transassist/moroccan-nuts-image-prompts.git image-prompts
+
+echo "Repository cloned successfully to /Users/tarikkhachiaa/Moroccan_Nuts_Website/image-prompts/"
